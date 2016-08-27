42332a234fcdfb2f6e04a86bca197f822f764abc
Mikkel Mouridsen <mikkelmouridsen@me.com>
2016-08-27 11:20:16 +0200

    Added basic interaction :D in PlayerController.cs

--- LD36/Assets/Scripts/PlayerController.cs
+++ LD36/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    public float speed = 10;
+    public Vector3 target;
+
+    public bool walking = false;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +15,35 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetMouseButtonDown(0))
+        {
+            Click();
+        }
+
+        if (walking)
+        {
+            float step = speed * Time.deltaTime;
+            if (this.transform.position.x != target.x)
+            {
+                this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector2(target.x, this.transform.position.y), step);
+            }
+            else
+            {
+                walking = false;
+            }
+        }
+
 	}
+
+    void Click()
+    {
+        int layerMask = 1 << 8;
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero, 10000f, layerMask);
+        if (hit)
+        {
+            Debug.Log(hit.transform.tag);
+            target = hit.transform.position;
+            walking = true;
+        }
+    }
 }

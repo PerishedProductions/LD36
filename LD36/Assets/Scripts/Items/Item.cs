fd5479a5b8138b3b4db232a4246fe307a614ead7
Mikkel Mouridsen <mikkelmouridsen@me.com>
2016-08-27 19:09:51 +0200

    Inventory now works :D

---
+++ LD36/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    public Sprite itemIcon;
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+
+    }
+
+    protected virtual void Awake()
+    {
+
+    }
+
+    protected virtual void OnGUI()
+    {
+
+    }
+
+    public virtual void SelectItem()
+    {
+        Debug.Log("Select base item!");
+    }
+
+    public virtual void UseItem()
+    {
+        Debug.Log("Using base item!");
+    }
+}
---
+++ LD36/Assets/Scripts/Items/Slot.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Slot : MonoBehaviour {
+
+    public Item item;
+    public Image image;
+
+    void Start()
+    {
+        image = this.GetComponent<Image>();
+    }
+
+    public void ChangeIcon()
+    {
+        if (item != null)
+        {
+            image.sprite = item.itemIcon;
+            image.color = new Color(255, 255, 255, 255);
+        }
+    }
+}

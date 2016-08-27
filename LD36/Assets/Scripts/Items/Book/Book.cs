d8d6abd511a868c283b85e8df76c044c3ed77033
THEXG4MING <emilsolo@sol.dk>
2016-08-27 23:36:03 +0200

    BOOKS ADDED

---
+++ LD36/Assets/Scripts/Items/Book/Book.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Book : Item
+{
+
+    public override void SelectItem()
+    {
+        Debug.Log("Selected Book glass!");
+    }
+
+    public override void UseItem()
+    {
+        Debug.Log("Using Book glass!");
+    }
+}
--- LD36/Assets/Scripts/StartScript.cs
+++ LD36/Assets/Scripts/StartScript.cs
@@ -3,10 +3,15 @@ using System.Collections;
 
 public class StartScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Awake () {
+    public Item item;
+
+    // Use this for initialization
+    void Awake () {
         GameManager.Instance.GetPlayer();
         GameManager.Instance.GetUI();
+        GameManager.Instance.GetInventory().AddItem(item);
+        item = null;
         Destroy(this.gameObject);
 	}
+
 }

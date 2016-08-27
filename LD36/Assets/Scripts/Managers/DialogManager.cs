0944bde06a544130d99f982e974e01dc17622b1f
Mikkel Mouridsen <mikkelmouridsen@me.com>
2016-08-27 12:18:00 +0200

    Added a DialogManager.cs singleton :D

---
+++ LD36/Assets/Scripts/Managers/DialogManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogManager : MonoBehaviour {
+
+    private static DialogManager _instance;
+
+    public static DialogManager Instance
+    {
+
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("DialogManager");
+                go.AddComponent<DialogManager>();
+            }
+
+            return _instance;
+        }
+
+    }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    void Dialog(string actor, string text)
+    {
+        
+    }
+
+}

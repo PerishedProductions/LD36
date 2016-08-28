715c0cdf493a04adc8e77bc2a8e4b1d2b7c20eb9
Mikkel Mouridsen <mikkelmouridsen@me.com>
2016-08-28 14:54:05 +0200

    More Level design

---
+++ LD36/Assets/Scripts/Interactable objects/MummyBox/MummyBox.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MummyBox : Interactable {
+    
+    public override void Interact()
+    {
+        DialogManager.Instance.Dialog("You open up the Sarcophagus", 0.04f);
+        base.Interact();
+    }
+
+    public override void Interact(Item Useditem)
+    {
+        if( Useditem is MagnifyingGlass )
+        {
+            DialogManager.Instance.Dialog("After closer examination you have concluded that this is Sarcophagus.", 0.04f);
+        }
+        else
+        {
+            base.Interact(Useditem);
+        }
+    }
+}

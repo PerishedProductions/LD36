2a2cae07ab54979dd7c500f5ae925a3349a52c4b
Dragion <Dragion147@hotmail.com>
2016-08-28 19:44:36 +0200

    Added CodeDoor and Button. buttons are linked with a codedoor and send a character to the door towards unlocking it.

---
+++ LD36/Assets/Scripts/Interactable objects/Button/Button.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : Interactable {
+
+    public char character;
+    public CodeDoor LinkedDoor;
+
+    public override void Interact()
+    {
+        DialogManager.Instance.Dialog("You pressed a weird button.", 0.04f);
+        if( !LinkedDoor )
+        {
+            LinkedDoor.InsertKey(character);
+        }
+    }
+}
---
+++ LD36/Assets/Scripts/Interactable objects/ScreenTransitionCursor/CodeDoor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CodeDoor : Door {
+
+    public string code;
+    private int progress =  0;
+
+    public void InsertKey( char key )
+    {
+        if( code[progress] == key && Locked )
+        {
+            DialogManager.Instance.Dialog("You hear a faint noise of a mechanism releasing a gear.", 0.02f);
+            progress++;
+            if( progress == code.Length)
+            {
+                DialogManager.Instance.Dialog("You hear a loud noise coming from the door.", 0.04f);
+                Locked = false;
+            }
+        }
+        else
+        {
+            DialogManager.Instance.Dialog("A faint noise of a mechanism falling back into its place echoed across the room.", 0.02f);
+            progress = 0;
+        }
+    }
+}

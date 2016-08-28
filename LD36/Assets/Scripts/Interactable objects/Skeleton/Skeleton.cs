4b361906c5f8211407551d2d49be88cd9d1a0003
Mikkel Mouridsen <mikkelmouridsen@me.com>
2016-08-28 21:23:35 +0200

    Added skeleton with the torch

---
+++ LD36/Assets/Scripts/Interactable objects/Skeleton/Skeleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skeleton : Interactable
+{
+
+    public override void Interact()
+    {
+        DialogManager.Instance.Dialog("You search the skeleton.", 0.04f);
+        base.Interact();
+    }
+
+    public override void Interact(Item Useditem)
+    {
+        if (Useditem is MagnifyingGlass)
+        {
+            DialogManager.Instance.Dialog("After closer examination you have concluded that this is a dead Skeleton.", 0.04f);
+        }
+        else
+        {
+            base.Interact(Useditem);
+        }
+    }
+}
